d3bf3af661b7ad6a773a013e769d8fbba51f9c60
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2025-01-05 14:03:05 -0300

    FEAT: base home page

--- src/pages/DetalhamentoDenunciaPage/DetalhamentoDenunciaPage.jsx
+++ src/pages/DetalhamentoDenunciaPage/DetalhamentoDenunciaPage.jsx
@@ -3,7 +3,7 @@ import "./DetalhamentoDenunciaPage.css"
 export default function DetalhamentoDenunciaPage() {
   return (
     <>
-      
+       <h1>Detalhamentoo denuncia page</h1>
     </>
   )
 }
