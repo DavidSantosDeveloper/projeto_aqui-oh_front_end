10274cf266cfc39b9d0c7a2459cfa80ec335c53e
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2025-01-05 16:08:29 -0300

    FEAT: base elementos pagina detalhamento

--- src/pages/DetalhamentoDenunciaPage/DetalhamentoDenunciaPage.jsx
+++ src/pages/DetalhamentoDenunciaPage/DetalhamentoDenunciaPage.jsx
@@ -1,9 +1,20 @@
 import "./DetalhamentoDenunciaPage.css"
+import a from "../../shared/components/Header/Header.jsx"
+import Header from "../../shared/components/Header/Header.jsx"
+import FiltroDenuncias from "../../shared/components/FiltroDenuncias/FiltroDenuncias.jsx"
+import Footer from "../../shared/components/footer/Footer.jsx"
 
 export default function DetalhamentoDenunciaPage() {
   return (
     <>
-       <h1>Detalhamentoo denuncia page</h1>
+       <Header></Header>
+       <FiltroDenuncias></FiltroDenuncias>
+        <div>
+            <h3>Chat de denuncia</h3>
+        </div>
+      <Footer></Footer>
+
+       
     </>
   )
 }
