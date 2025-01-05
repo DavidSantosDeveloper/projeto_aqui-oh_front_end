2c36cc9f39aeb8a556f86ad0b9b506091fc92aa3
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2025-01-05 13:53:22 -0300

    FEAT: base da pagina CadrastoPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LoginPage from './pages/LoginPage/LoginPage.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage></LoginPage>
-    
+    <App></App>
+
   </StrictMode>,
 )
---
+++ src/pages/CadrastroPage/CadrastroPage.jsx
@@ -0,0 +1,9 @@
+import "./CadrastroPage.css"
+
+export default function CadrastroPage() {
+  return (
+    <>
+      <h1>Pagina de cadrastro</h1>
+    </>
+  )
+}
