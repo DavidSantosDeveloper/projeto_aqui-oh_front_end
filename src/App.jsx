b55df3380ad4ca394c8a65b66de00e19fe9ac042
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2025-01-05 14:26:02 -0300

    FEAT: rotas entre as paginas funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import "./pages/CadrastroPage/"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+
+import "./pages/ChatDenunciaPage/ChatDenunciaPage.jsx"
+import "./pages/DetalhamentoDenunciaPage/DetalhamentoDenunciaPage.jsx"
+import "./pages/HomePage/HomePage.jsx"
+import "./pages/LoginPage/LoginPage.jsx"
 
 import './App.css'
+import HomePage from "./pages/HomePage/HomePage.jsx"
+import LoginPage from "./pages/LoginPage/LoginPage.jsx"
+import CadrastroPage from "./pages/CadrastroPage/CadrastroPage.jsx"
+
 
 function App() {
  
@@ -8,7 +18,16 @@ function App() {
   return (
     <>
     
-    
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<LoginPage></LoginPage>}></Route>
+              <Route path="/cadrastro" element={<CadrastroPage></CadrastroPage>}></Route>
+              <Route path="/chat" element={<ChatDenunciaPage></ChatDenunciaPage>}></Route>
+              <Route path="/detalhamento" element={<DetalhamentoDenunciaPage></DetalhamentoDenunciaPage>}></Route>
+              <Route path="/home" element={<HomePage></HomePage>}></Route>
+          </Routes>
+      
+      </BrowserRouter>
     
     
     </>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import "./HomePage.css"
 export default function HomePage() {
   return (
     <>
-    
+        <h1>Hello homepage</h1>
       
     </>
   )
